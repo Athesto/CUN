3fd808a41edcfc7081f7e95266e2f31591686a46
Gustavo Adolfo Mejía Sánchez <gamez.live@gmail.com>
2025-01-02 21:50:20 -0500

    rename function names

--- cursos/fundamentos-de-programacion/ACA-1-script.py
+++ cursos/fundamentos-de-programacion/ACA-1-script.py
@@ -4,7 +4,7 @@ import unittest
 from unittest.mock import patch
 
 # Ejercicio 3a: Determinar si el numero es positivo, cero o negativo
-def solution_3a():
+def classify_number():
     data_input = input("Enter a number: ")
     try:
         number = float(data_input)
@@ -22,7 +22,7 @@ def solution_3a():
     return 'negative'
 
 # Ejercicio 3b: Determinar el IMC
-def solution_3b():
+def get_imc():
     try: 
         weight = float(input("Peso (kg): "))
         height = float(input("Altura (m): "))
@@ -52,7 +52,7 @@ def solution_3b():
     }
 
 # Ejercicio 3c: Calcular el factorial de un numero
-def solution_3c(number):
+def get_factorial(number):
     if number == 0:
         return 1
     for i in range(1, number):
@@ -60,7 +60,7 @@ def solution_3c(number):
     return number
 
 # Ejercicio 3d: tabla de multiplicar
-def solution_3d():
+def mutiplication_table():
     multiplier = 1
     multiplicand = 0
     while multiplier <= 10:
@@ -74,10 +74,10 @@ def solution_3d():
 class Tests(unittest.TestCase):
     @patch('builtins.input', side_effect=['1', '0', '-1', "hello world"])
     def test_3a(self, mock_input):
-        self.assertEqual(solution_3a(), 'positive')
-        self.assertEqual(solution_3a(), 'zero')
-        self.assertEqual(solution_3a(), 'negative')
-        self.assertEqual(solution_3a(), 'Error')
+        self.assertEqual(classify_number(), 'positive')
+        self.assertEqual(classify_number(), 'zero')
+        self.assertEqual(classify_number(), 'negative')
+        self.assertEqual(classify_number(), 'Error')
 
     @patch('builtins.input', side_effect=[
         '60', '2',
@@ -87,18 +87,18 @@ class Tests(unittest.TestCase):
         "hello world", "2",
     ])
     def test_3b(self, mock_input):
-        self.assertEqual(solution_3b(), {'imc': 15.0, 'message': 'Bajo peso'})
-        self.assertEqual(solution_3b(), {'imc': 20.0, 'message': 'Peso normal'})
-        self.assertEqual(solution_3b(), {'imc': 25.0, 'message': 'Sobrepeso'})
-        self.assertEqual(solution_3b(), {'imc': 40.0, 'message': 'Obesidad III'})
-        self.assertEqual(solution_3b(), "Error") 
+        self.assertEqual(get_imc(), {'imc': 15.0, 'message': 'Bajo peso'})
+        self.assertEqual(get_imc(), {'imc': 20.0, 'message': 'Peso normal'})
+        self.assertEqual(get_imc(), {'imc': 25.0, 'message': 'Sobrepeso'})
+        self.assertEqual(get_imc(), {'imc': 40.0, 'message': 'Obesidad III'})
+        self.assertEqual(get_imc(), "Error") 
 
     def test_3c(self):
-        self.assertEqual(solution_3c(5), 120)
-        self.assertEqual(solution_3c(0), 1)
+        self.assertEqual(get_factorial(5), 120)
+        self.assertEqual(get_factorial(0), 1)
 
     def test_3d(self):
-        self.assertEqual(solution_3d(), None)
+        self.assertEqual(mutiplication_table(), None)
 
 
 if __name__ == '__main__':
